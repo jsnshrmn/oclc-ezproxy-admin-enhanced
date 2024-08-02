45322ce78b89e33d0cc015b5bb74d9baa2e1e435
jsn <jsherman@wikimedia.org>
2024-08-02 16:52:36 -0500

    remove some boilerplate

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -1,11 +1,11 @@
 <template>
-  <object id="admin" type="text/html" data="/admin" width="100%"></object>
+    <object id="admin" type="text/html" data="/admin" width="100%"></object>
 </template>
 
 <style>
 #admin {
   display: flex;
-  width: 830px;
+  width: 100%;
   background: #fff;
   min-height: 100vh;
   display: flex;
